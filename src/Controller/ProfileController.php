ea0c617b4a1565bd4aea754a7738e9aae722e1c6
vadym <vadym.tarasov.91@gmail.com>
2022-08-08 16:35:36 +0300

    edit page added

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\ProfileEditFormType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +16,26 @@ class ProfileController extends AbstractController
     {
         return $this->render('main/profile/index.html.twig', [
             'controller_name' => 'ProfileController',
+        ]);
+    }
+
+    #[Route('/profile/edit', name: 'main_profile_edit')]
+    public function edit(Request $request, ManagerRegistry $doctrine):Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileEditFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('main_profile_index');
+        }
+
+        return $this->render('main/profile/edit.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
