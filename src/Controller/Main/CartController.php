2563c896a91451b3de104314e70d3bbfb30df108
Vadym <vadym.tarasov.91@gmail.com>
2022-09-13 16:43:43 +0300

    Forming Order from Cart

--- src/Controller/Main/CartController.php
+++ src/Controller/Main/CartController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Main;
 
 use App\Repository\CartRepository;
 //use App\Utils\Manager\OrderManager;
+use App\Utils\Manager\OrderManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,16 +26,17 @@ class CartController extends AbstractController
         ]);
     }
 
-//    /**
-//     * @Route("/cart/create", name="main_cart_create")
-//     */
-//    public function create(Request $request, OrderManager $orderManager): Response
-//    {
-//        $phpSessionId = $request->cookies->get('PHPSESSID');
-//        $user = $this->getUser();
-//
-//        $orderManager->createOrderFromCartBySessionId($phpSessionId, $user);
-//
-//        return $this->redirectToRoute('main_cart_show');
-//    }
+    /**
+     * @Route("/cart/create", name="main_cart_create")
+     */
+    public function create(Request $request, OrderManager $orderManager): Response
+    {
+        $phpSessionId = $request->cookies->get('PHPSESSID');
+        $user = $this->getUser();
+//        dd($user);
+
+        $orderManager->createOrderFromCartBySessionId($phpSessionId, $user);
+
+        return $this->redirectToRoute('main_cart_show');
+    }
 }
---
+++ src/Utils/Manager/OrderManager.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Utils\Manager;
+
+use App\Entity\Cart;
+use App\Entity\Order;
+use App\Entity\OrderProduct;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectRepository;
+
+class OrderManager extends AbstractBaseManager
+{
+
+    private CartManager $cartManager;
+
+    public function __construct(EntityManagerInterface $entityManager, CartManager $cartManager)
+    {
+        parent::__construct($entityManager);
+        $this->cartManager = $cartManager;
+    }
+
+    /**
+     * @return ObjectRepository
+     */
+    public function getRepository(): ObjectRepository
+    {
+        return $this->entityManager->getRepository(Order::class);
+    }
+
+    /**
+     * @param string $sessionId
+     * @param User $user
+     * @return void
+     */
+    public function createOrderFromCartBySessionId(string $sessionId, User $user)
+    {
+        $cart = $this->cartManager->getRepository()->findOneBy(['sessionId' => $sessionId]);
+        if ($cart) {
+            $this->createOrderFromCart($cart, $user);
+        }
+    }
+
+    public function createOrderFromCart(Cart $cart, User $user)
+    {
+//        dd($user);
+
+        $order = new Order();
+        $order->setOwner($user);
+        $order->setStatus(0);
+        $orderTotalPrice = 0;
+
+
+        foreach ($cart->getCartProducts()->getValues() as $cartProduct) {
+            $product = $cartProduct->getProduct();
+
+            $orderProduct = new OrderProduct();
+            $orderProduct->setAppOrder($order);
+            $orderProduct->setQuantity($cartProduct->getQuantity());
+            $orderProduct->setPricePerOne($product->getPrice());
+            $orderProduct->setProduct($product);
+
+            $orderTotalPrice += $orderProduct->getQuantity() * $orderProduct->getPricePerOne();
+
+
+            $order->addOrderProduct($orderProduct);
+
+            $this->entityManager->persist($orderProduct);
+        }
+
+
+
+        $order->setTotalPrice($orderTotalPrice);
+//        $order->setUpdatedAt(new \DateTimeImmutable());
+        $this->entityManager->persist($order);
+
+        dd($order);
+
+
+    }
+
+
+    /**
+     * @param Order $order
+     */
+    public function remove(object $order)
+    {
+        $order->setIsDeleted(true);
+
+
+        $this->save($order);
+    }
+}
